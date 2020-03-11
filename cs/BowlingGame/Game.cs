8d127d147eb775ebaa5eafc22a0be76829194604
Vlad <hrrrrustic@gmail.com>
2020-03-11 10:48:40 +0300

    Some tests

--- cs/BowlingGame/Game.cs
+++ cs/BowlingGame/Game.cs
@@ -1,19 +1,46 @@
 ﻿using System;
+using System.Xml.Linq;
 using BowlingGame.Infrastructure;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace BowlingGame
 {
+    public class GameRound
+    {
+        public bool IsSpare { get; set; }
+        public bool IsStrike { get; set; }
+        public int RollOne { get; set; }
+        public int RollSecond { get; set; }
+
+    }
     public class Game
     {
+        public Game()
+        {
+            for (int i = 0; i < rounds.Length; i++)
+            {
+                rounds[i] = new GameRound();
+            }
+        }
+        private readonly Int32 maxPins = 10;
+        public int Score { get; set; }
+        public int CurrentRound { get; set; }
+        
+        public readonly GameRound[] rounds = new GameRound[10];
+
         public void Roll(int pins)
         {
+            if(pins < 0 || pins > 10)
+                throw new ArgumentException();
+
+            if (pins == 10)
+                rounds[CurrentRound].IsStrike = true;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            return Score;
         }
     }
 
@@ -27,5 +54,25 @@ namespace BowlingGame
                 .GetScore()
                 .Should().Be(0);
         }
+        [Test]
+        public void RollNegative_ArgumentException()
+        {
+            Action act = () => new Game().Roll(-1);
+            act.ShouldThrow<ArgumentException>();
+        }
+        [Test]
+        public void RollMoreTenPins_ArgumentException()
+        {
+            Action act = () => new Game().Roll(11);
+            act.ShouldThrow<ArgumentException>();
+        }
+        [Test]
+        public void RollAllPinsInOneRoll_GameRoundIsStrike()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.rounds[0].IsStrike.Should().Be(true);
+        }
+
     }
 }
